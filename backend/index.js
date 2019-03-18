39df3170812fd71aed65f4960d5db89a2674d175
Vitalii_Posidailo <vitalii_posidailo@epam.com>
2019-03-18 15:24:44 +0100

    MoviewDB - Task-1: Changed port for backend. Added application start command.

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => res.send('Movie backend'))
-app.listen(3000, () => console.log('Listening on port 3000!'))
+app.listen(3001, () => console.log('Listening on port 3001!'))
